ee241bad25851769d127639981e26af5ba79420c
Gergely Brautigam <skarlso777@gmail.com>
2021-05-31 14:47:26 +0200

    Adding new command update and testing tag overwrite

--- cmd/mod.go
+++ cmd/mod.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -15,14 +18,27 @@ var (
 		Short: "Update the modules file",
 		Run:   runModUpdateCmd,
 	}
+	updateArgs struct {
+		listRepositoryShorts bool
+	}
 )
 
 func init() {
 	RootCmd.AddCommand(modCmd)
 	modCmd.AddCommand(updateCmd)
+
+	f := updateCmd.PersistentFlags()
+	// Persistent flags
+	f.BoolVarP(&updateArgs.listRepositoryShorts, "repositories", "r", false, "List all available repository shorts for update command.")
 }
 
 // runModUpdateCmd .
 func runModUpdateCmd(cmd *cobra.Command, args []string) {
-
+	if updateArgs.listRepositoryShorts {
+		fmt.Println("The following repository shorts are available:")
+		for k, v := range repositories {
+			fmt.Printf("	%s = %s\n", k, v)
+		}
+		os.Exit(0)
+	}
 }
